4658a5bac7b5d18c385b780cf811c1a7a739636a
jorwsmit <jorwsmith92@gmail.com>
2020-09-23 10:09:12 -0400

    Encounters audit updates.

---
+++ force-app/main/default/lwc/testLWC/testLWC.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class TestLWC extends LightningElement {
+    @api recordId;
+}
---
+++ force-app/main/default/lwc/unitContactSheet/unitContactSheet.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class UnitContactSheet extends LightningElement {
+    @api recordId;
+}
